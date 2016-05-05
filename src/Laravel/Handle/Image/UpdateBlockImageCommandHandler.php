b5b764d32aab5ed77a507092d4d8c5ba8f046448
Boris Dorofeev <boris.dorofeev2014@yandex.ru>
2016-05-05 16:31:03 +0600

    Заглушка для SVG

--- src/Laravel/Handle/Image/UpdateBlockImageCommandHandler.php
+++ src/Laravel/Handle/Image/UpdateBlockImageCommandHandler.php
@@ -1,5 +1,6 @@
 <?php namespace Interpro\QuickStorage\Laravel\Handle\Image;
 
+use Illuminate\Support\Facades\App;
 use Interpro\ImageFileLogic\Laravel\Action\CreateImageAction;
 use Interpro\QuickStorage\Concept\Command\Image\UpdateBlockImageCommand;
 use Interpro\QuickStorage\Laravel\Item\ImageItem;
@@ -14,17 +15,41 @@ class UpdateBlockImageCommandHandler extends ImageCommandHandler
     {
 
         $config_name = $command->block_name.'_'.$command->image_name;
+        $extension = $command->req_orig_file->getClientOriginalExtension();
+        //Заглушка для векторных форматов, где не надо ни ресайзить ни кропить, один файл для всех назначений
+        if($extension == 'svg'){
 
-        $fields = $this->imageRepository->getBlockImage($command->block_name, $command->image_name);
+            $pathResolver = App::make('Interpro\ImageFileLogic\Concept\PathResolver');
+            $report       = App::make('Interpro\ImageFileLogic\Concept\Report');
 
-        $imageItem = new ImageItem($config_name.'_0', $fields);
+            $images_dir = $pathResolver->getImageDir();
 
-        $headAction = new CreateImageAction($command->req_orig_file);
+            $new_image_name = $config_name.'_0.svg';
 
-        //К голове присоединяется остальная цепочка
-        $this->actionChainFactory->buildChain($headAction, 'update', $config_name);
+            $command->req_orig_file->move(
+                $images_dir,
+                $new_image_name
+            );
 
-        $headAction->applyFor($imageItem);
+            $report->setImageResize($config_name.'_0', 'preview',   $new_image_name);
+            $report->setImageResize($config_name.'_0', 'original',  $new_image_name);
+            $report->setImageResize($config_name.'_0', 'primary',   $new_image_name);
+            $report->setImageResize($config_name.'_0', 'secondary', $new_image_name);
+            $report->setImageResize($config_name.'_0', 'icon',      $new_image_name);
+
+        }else{
+
+            $fields = $this->imageRepository->getBlockImage($command->block_name, $command->image_name);
+
+            $imageItem = new ImageItem($config_name.'_0', $fields);
+
+            $headAction = new CreateImageAction($command->req_orig_file);
+
+            //К голове присоединяется остальная цепочка
+            $this->actionChainFactory->buildChain($headAction, 'update', $config_name);
+
+            $headAction->applyFor($imageItem);
+        }
 
     }
 
--- src/Laravel/Handle/Image/UpdateOneGroupImageCommandHandler.php
+++ src/Laravel/Handle/Image/UpdateOneGroupImageCommandHandler.php
@@ -1,5 +1,6 @@
 <?php namespace Interpro\QuickStorage\Laravel\Handle\Image;
 
+use Illuminate\Support\Facades\App;
 use Interpro\ImageFileLogic\Laravel\Action\CreateImageAction;
 use Interpro\QuickStorage\Concept\Command\Image\UpdateOneGroupImageCommand;
 use Interpro\QuickStorage\Laravel\Item\ImageItem;
@@ -14,17 +15,41 @@ class UpdateOneGroupImageCommandHandler extends ImageCommandHandler
     {
 
         $config_name = $command->group_name.'_'.$command->image_name;
+        $extension = $command->req_orig_file->getClientOriginalExtension();
+        //Заглушка для векторных форматов, где не надо ни ресайзить ни кропить, один файл для всех назначений
+        if($extension == 'svg'){
 
-        $fields = $this->qSource->oneImageQueryForGroup($command->block_name, $command->group_name, $command->group_id, $command->image_name);
+            $pathResolver = App::make('Interpro\ImageFileLogic\Concept\PathResolver');
+            $report       = App::make('Interpro\ImageFileLogic\Concept\Report');
 
-        $imageItem = new ImageItem($config_name.'_'.$command->group_id, $fields);
+            $images_dir = $pathResolver->getImageDir();
 
-        $headAction = new CreateImageAction($command->req_orig_file);
+            $new_image_name = $config_name.'_'.$command->group_id.'.svg';
 
-        //К голове присоединяется остальная цепочка
-        $this->actionChainFactory->buildChain($headAction, 'update', $config_name);
+            $command->req_orig_file->move(
+                $images_dir,
+                $new_image_name
+            );
 
-        $headAction->applyFor($imageItem);
+            $report->setImageResize($config_name.'_'.$command->group_id, 'preview',   $new_image_name);
+            $report->setImageResize($config_name.'_'.$command->group_id, 'original',  $new_image_name);
+            $report->setImageResize($config_name.'_'.$command->group_id, 'primary',   $new_image_name);
+            $report->setImageResize($config_name.'_'.$command->group_id, 'secondary', $new_image_name);
+            $report->setImageResize($config_name.'_'.$command->group_id, 'icon',      $new_image_name);
+
+        }else{
+
+            $fields = $this->qSource->oneImageQueryForGroup($command->block_name, $command->group_name, $command->group_id, $command->image_name);
+
+            $imageItem = new ImageItem($config_name.'_'.$command->group_id, $fields);
+
+            $headAction = new CreateImageAction($command->req_orig_file);
+
+            //К голове присоединяется остальная цепочка
+            $this->actionChainFactory->buildChain($headAction, 'update', $config_name);
+
+            $headAction->applyFor($imageItem);
+        }
 
     }
 
